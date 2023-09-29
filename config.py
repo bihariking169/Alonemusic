6a637366295404d78e276ecfd85824a27e560bb3
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-09-29 21:43:48 +0530

    BRANDED SUPPORT

--- config.py
+++ config.py
@@ -13,15 +13,15 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/9db6ae360e0a7b833abff.jpg")
-START_IMG = getenv("START_IMG", "https://te.legra.ph/file/3d0d7d23d3a7fb86b442e.jpg")
+PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/bfd89605046c8eb374004.jpg")
+START_IMG = getenv("START_IMG", "https://te.legra.ph/file/ed6b22a4cc0b0bdb9fe71.jpg")
 
 SESSION = getenv("SESSION", None)
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/BRANDED_WORLD")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/BRANDRD_BOT")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/The_flirting_zonne")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/alone_happy_worlld")
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
 
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "KHUSHI_MUSIC76_BOT")  
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "𝆺𝅥⃝🍃 ⃪ͥ͢ ᷟ 𝐀ℓσиє𓆪•┼⃖‌ꭗ🎋")  
 
-FAILED = "https://te.legra.ph/file/6a4ac076ff159bdcf9656.jpg"
+FAILED = "https://te.legra.ph/file/664e77b17f2b7b56d8893.jpg"
