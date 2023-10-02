8cf227b986c5809c850d9c2791466b9671e96e3a
ALONE HAPPY <144148196+bihariking169@users.noreply.github.com>
2023-10-02 16:31:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,15 +13,15 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID","6469850034"))
 
-PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/168af61a3b8fc03bd3166.jpg")
-START_IMG = getenv("START_IMG", "https://te.legra.ph/file/168af61a3b8fc03bd3166.jpg")
+PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/c8d197e5f9daed42e9726.jpg")
+START_IMG = getenv("START_IMG", "https://te.legra.ph/file/c8d197e5f9daed42e9726.jpg")
 
 SESSION = getenv("SESSION", None)
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/The_flirting_zonne")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/The_Fllirting_Zone")
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/alone_happy_worlld")
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
 
 MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "𝆺𝅥⃝🍃 ⃪ͥ͢ ᷟ 𝐀ℓσиє𓆪•┼⃖‌ꭗ🎋")  
 
-FAILED = "https://te.legra.ph/file/168af61a3b8fc03bd3166.jpg"
+FAILED = "https://te.legra.ph/file/c8d197e5f9daed42e9726.jpg"
