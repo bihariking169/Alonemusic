4202d67b55cab07257ab20178ccf9d6d3d9f4552
ALONE HAPPY <144148196+bihariking169@users.noreply.github.com>
2023-10-09 07:07:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,10 +11,10 @@ API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
-OWNER_ID = int(getenv("OWNER_ID","6562765473"))
+OWNER_ID = int(getenv("OWNER_ID","2049079755"))
 
-PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/3f1843d16963f652513f5.jpg")
-START_IMG = getenv("START_IMG", "https://te.legra.ph/file/3f1843d16963f652513f5.jpg")
+PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/e145d30b30480fc0abb5e.jpg")
+START_IMG = getenv("START_IMG", "https://te.legra.ph/file/e145d30b30480fc0abb5e.jpg")
 
 SESSION = getenv("SESSION", None)
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/The_Fllirting_Zone")
